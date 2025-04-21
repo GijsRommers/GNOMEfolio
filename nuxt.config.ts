121f2f9d44226e0e8f69f571f560d01c29bc44da
Gijs Rommers <contactnanogijs@gmail.com>
2025-04-21 16:06:08 +0200

    Added base tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss'
   ]
 })
