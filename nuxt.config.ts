fb45f8098c4f0ce29d3fa97f160a578e99b529b9
Gijs Rommers <contactnanogijs@gmail.com>
2025-04-23 23:23:09 +0200

    Static maybe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare_pages',
+    preset: 'static',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
